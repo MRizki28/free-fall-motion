0ae3e4c311bf7948c63f40282dafa7d7b58e422a
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-03-05 22:37:33 +0800

    finish

---
+++ perpindahanBenda.py
@@ -0,0 +1,12 @@
+import math
+
+def perpindahanBenda(t,g):
+    vt = 1/2 * g * t * t
+    
+    return vt
+
+g = 9.8
+t = float(input('Masukkan waktu tempuh (detik):'))
+
+hasil = perpindahanBenda(t,g)
+print("Sehingga perpindahan benda selama", t, '=', hasil,'m')
