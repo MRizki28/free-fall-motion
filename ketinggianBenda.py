f739faf95ba0ae3cf5bf15ff5e34b79699754b49
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-03-06 19:06:46 +0800

    finish

--- ketinggianBenda.py
+++ ketinggianBenda.py
@@ -1,4 +1,4 @@
-def ketenggianBenda(t, g, h0):
+def ketinggianBenda(t, g, h0):
     ht = h0 - 0.5 * g * t * t
     return ht
 
@@ -7,5 +7,5 @@ g = 9.8
 h0 = float(input('Masukkan tinggi awal (meter): '))
 t = float(input('Masukkan waktu (detik): '))
 
-hasil = ketenggianBenda(t, g, h0)
-print('Ketinggian benda setelah', t, 'detik adalah', hasil, 'meter.')
+hasil = ketinggianBenda(t, g, h0)
+print('Ketinggian benda setelah', t, 'detik adalah', abs(hasil), 'meter.')
